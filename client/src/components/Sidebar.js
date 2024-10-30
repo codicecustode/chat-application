ed42e4c2868451a05c92e2ba07dd1c493d006699
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-10-30 22:39:40 +0530

    Refactor Sidebar component to use Tab.Container for improved navigation

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,19 +1,33 @@
 // Sidebar.js
 import React,{ useState } from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Tab } from 'react-bootstrap'
+import { Contacts } from './Contact'
+import { Conversations } from './Conversation'
 
 const Sidebar = () => {
-    const [activeKey, setActivekey] = useState('contact')
+    const [activeKey, setActiveKey] = useState('contact')
   return (
-    <div className="flex-column" style={{height:"100vh", width:"100vw"}}>
-        <Nav activeKey={activeKey} variant="tabs" >
-            <Nav.Item>
-                <Nav.Link onClick={()=>setActivekey('contact')} eventKey="contact">Contact</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link onClick={()=>setActivekey('conversation')}eventKey="conversation" >Conversation</Nav.Link>
-            </Nav.Item>
-        </Nav>
+    <div className="flex-column" style={{height:"100vh", width:"30vw", overflowY:"auto"}}>
+        <Tab.Container activeKey={activeKey} onSelect={(k) => setActiveKey(k)}>
+            <Nav variant="tabs" style={{dispaly:"flex", justifyContent:"space-around"}}>
+                <Nav.Item>
+                    <Nav.Link eventKey="contact">Contact</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="conversation">Conversation</Nav.Link>
+                </Nav.Item>
+            </Nav>
+
+            <Tab.Content style={{borderRight: "1px solid #ccc",flexGrow:"1"}}>
+                <Tab.Pane eventKey="contact">
+                    <Contacts />
+                </Tab.Pane>
+                <Tab.Pane eventKey="conversation">
+                    <Conversations />
+                </Tab.Pane>
+            </Tab.Content>
+        </Tab.Container>
+
     </div>
   );
 };
