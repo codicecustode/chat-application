fab4f0e839174948657a8c9ba968181dea4a1ab7
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-11-06 00:05:50 +0530

    Refactor NewContact component to use ModelShowContext for managing model show state and handle show/hide

--- client/src/components/contactModel.js
+++ client/src/components/contactModel.js
@@ -1,6 +1,9 @@
 
+import { useModelShow } from '../context/ModelShowContext';
 
-export const NewContact = ({handleHide}) =>{
+export const NewContact = () => {
+
+    const { handleHide } = useModelShow();
 
     const handleSubmit = (e) => {
         e.prevendefault();
@@ -15,19 +18,19 @@ export const NewContact = ({handleHide}) =>{
 
     return (
         <div className="new-contact" >
-            <form onSubmit={ handleSubmit }>
+            <form onSubmit={handleSubmit}>
                 <h3>Create New Entry</h3>
                 <label htmlFor="name">
                     Name:
-                    <input type="text" id="name" name="name" required placeholder="Enter your name" />
+                    <input type="text" id="name" name="name" ref={nameRef} required placeholder="Enter your name" />
                 </label>
                 <br />
                 <label htmlFor="id">
                     ID:
-                    <input type="text" id="id" name="id" required placeholder="Enter your ID" />
+                    <input type="text" id="id" name="id" ref={idRef} required placeholder="Enter your ID" />
                 </label>
                 <br />
-                <button type="submit">Save Entry</button>
+                <button type="submit">Create Contact</button>
                 <button type="button" onClick={handleHide}>Close</button>
             </form>
         </div>
