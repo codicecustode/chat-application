1930ddefa3ba98a92dd466f3a0f2b4a52881a685
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-11-06 00:25:59 +0530

    Refactor App component to use ModelShowProvider for managing model show state

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,14 @@
 import { useLocalStorage } from '../hooks/useLocalStorage';
 import { Login } from "./Login"
 import { Dashboard } from './Dashboard'
+import { ModelShowProvider } from '../context/ModelShowContext'
 function App() {
   const [id, setId] = useLocalStorage('id')
 
   return (
-    <>
+    <ModelShowProvider>
       {id ? <Dashboard id={id} /> : <Login onSubmitId={setId} />}
-    </>
+    </ ModelShowProvider>
   );
 }
 
