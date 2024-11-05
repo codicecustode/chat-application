ee418a86877d67ff0ad44d618061175dd5d51f44
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-11-06 00:06:04 +0530

    Refactor Dashboard component to use ModelShowContext for managing model show state and handle show/hide of NewContact component

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,25 @@
 import Sidebar from "./Sidebar"
-
+import { NewContact } from './contactModel'
+import { useModelShow } from '../context/ModelShowContext'
 export const Dashboard = (id) => {
-    return(
-        
-        <Sidebar id={ id }/>
+    const { show } = useModelShow();
+    return (
+        <>
+            <div style={{ display: "flex" }}>
+                <Sidebar id={id} />
 
+                { show && <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        width: "100%",         
+                        height: "100vh",   
+                    }}
+                >
+                    <NewContact />
+                </div> }
+            </div>
+        </>
     )
 }
