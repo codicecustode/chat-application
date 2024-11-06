3a753c091c1828702b85dc8320e26cfe351b498e
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-11-06 20:21:03 +0530

    Add ContactsProvider and context for managing contacts with local storage

---
+++ client/src/context/ContactsProvider.js
@@ -0,0 +1,25 @@
+import React, { createContext, useContext } from 'react'
+import { useLocalStorage } from '../hooks/useLocalStorage'
+
+export const ContactsContext = createContext()
+
+export const ContactsProvider = ({ children }) => {
+    const [contacts, setContacts] = useLocalStorage('contacts', [])
+
+    const createContact = (id, name) => {
+        setContacts(prevContacts => {
+            return [...prevContacts, { id, name }]
+        })
+    }
+
+    return (
+        <ContactsContext.Provider value={{ contacts, createContact }}>
+            {children}
+        </ContactsContext.Provider>
+    )
+}
+
+export const useContacts = () => {
+    return useContext(ContactsContext)
+}
+
