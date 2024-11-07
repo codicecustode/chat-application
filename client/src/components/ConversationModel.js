966d3793a7c9f8a6d0afac57189fd4dc8419d286
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-11-07 20:26:19 +0530

    Add NewConversation component for creating conversations with selected contacts

---
+++ client/src/components/ConversationModel.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { Modal, Button, Form } from 'react-bootstrap'
+import { useContacts } from '../context/ContactsProvider';
+import { useConversation } from '../context/ConversationProvider';
+export const NewConversation = () => {
+    const [selectedContactIds, setSelectedContactIds] = useState([])
+    const { contacts } = useContacts()
+    const { createConversation } = useConversation()
+
+    const handleCheckBoxChange = (contactId) => {
+        if (selectedContactIds.includes(contactId)) {
+            setSelectedContactIds(selectedContactIds.filter(id => id !== contactId))
+            } else {
+                setSelectedContactIds([...selectedContactIds, contactId])
+            }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // logic to create conversation
+        createConversation(selectedContactIds)
+    }
+
+
+    return (
+        <>
+            <Modal.Header>Create Conversation</Modal.Header>
+            <Modal.Body>
+
+                <Form onSubmit={handleSubmit}>
+                    {contacts.map(contact => (
+                        <Form.Group controlId={contact.id} key={contact.id}>
+                            <Form.Check
+                                type="checkbox"
+                                value={selectedContactIds.includes(contact.id)}
+                                label={contact.name}
+                                onChange={() => handleCheckBoxChange(contact.id)}
+                            />
+                        </Form.Group>
+                    ))}
+
+                    <Button type="submit">Create</Button>
+
+                </Form>
+
+            </Modal.Body>
+        </>
+
+    )
+}
+
