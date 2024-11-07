99717731472c21592f95e3052f1773acb46acd24
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-11-07 20:27:06 +0530

    Refactor App component to include ContactsProvider and ConversationProvider; remove NewContact component from Dashboard

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,19 @@ import { useLocalStorage } from '../hooks/useLocalStorage';
 import { Login } from "./Login"
 import { Dashboard } from './Dashboard'
 import { ModelShowProvider } from '../context/ModelShowContext'
+import { ContactsProvider} from '../context/ContactsProvider'
+import { ConversationProvider } from '../context/ConversationProvider'
 function App() {
   const [id, setId] = useLocalStorage('id')
 
   return (
-    <ModelShowProvider>
-      {id ? <Dashboard id={id} /> : <Login onSubmitId={setId} />}
-    </ ModelShowProvider>
+    <ContactsProvider>
+      <ConversationProvider>
+        <ModelShowProvider>
+          {id ? <Dashboard id={id} /> : <Login onSubmitId={setId} />}
+        </ ModelShowProvider>
+      </ConversationProvider>
+    </ContactsProvider>
   );
 }
 
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,26 +1,10 @@
 import Sidebar from "./Sidebar"
-import { NewContact } from './contactModel'
-import { useModelShow } from '../context/ModelShowContext'
-import { ContactsProvider} from '../context/ContactsProvider'
-export const Dashboard = (id) => {
-    const { show } = useModelShow();
-    return (
-        <ContactsProvider>
-            <div style={{ display: "flex" }}>
-                <Sidebar id={id} />
 
-                { show && <div
-                    style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        width: "100%",         
-                        height: "100vh",   
-                    }}
-                >
-                    <NewContact />
-                </div> }
-            </div>
-        </ContactsProvider>
+export const Dashboard = (id) => {
+    return (
+
+        <div style={{ display: "flex" }}>
+            <Sidebar id={id} />
+        </div>
     )
 }
