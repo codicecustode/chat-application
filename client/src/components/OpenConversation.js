7b5dbed22b17cbe476451f87d8c5612ef577b1dc
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-11-10 17:15:22 +0530

    Display messages in OpenConversation with sender identification and improved layout

--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -16,12 +16,39 @@ export const OpenConversation = () => {
         )
         setText('')
     }
+    console.log(selectedConversation)
     return (
         <div className="d-flex flex-column flex-grow-1">
-            <div className="flex-grow-1 overflow-auto"></div>
+            <div className="flex-grow-1 overflow-auto">
+                <div className="h-100 d-flex flex-column
+                align-items-start justify-content-end px-3">
 
-            <Form>
-                <Form.Group onSubmit={handleSubmit}>
+                    {
+                        selectedConversation.messages.map((message, index) => {
+                            return (
+                                <div key={index} className="mb-2 d-flex flex-column">
+                                    <div className={`text-break rounded px-2 py-1
+                                            ${message.fromMe ? 'bg-primary text-white' : 'border'}`}>
+                                        {message.text}
+                                    </div>
+                                    <div className={`text-break text-muted small ${message.fromMe ? 
+                                        'text-right' : ''
+                                    }`}>
+                                        {message.fromMe ? 'you' : message.sender.name}
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+        
+
+
+
+                </div>
+            </div>
+
+            <Form onSubmit={handleSubmit} style={{ width: '100%', overflow: 'hidden' }}>
+                <Form.Group style={{ width: '100%' }}>
                     <InputGroup className="m-3">
                         <Form.Control
                             as="textarea"
@@ -29,9 +56,9 @@ export const OpenConversation = () => {
                             value={text}
                             onChange={e => setText(e.target.value)}
                             required
-                            style={{ height: '75px', resize: 'none' }}
+                            style={{ height: '75px', resize: 'none',width: 'calc(100% - 80px)' }}
                         />
-                        <Button type="submit">Send</Button>
+                        <Button type="submit" style={{ width: '80px' }}>Send</Button>
                     </InputGroup>
                 </Form.Group>
 
