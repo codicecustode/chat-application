741949a8d062b40d17ff677646acc041a5d4fb4f
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-11-12 18:17:15 +0530

    Add showConversation state to Dashboard and pass prop to Sidebar

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,16 @@
+import React, { useState } from "react"
 import Sidebar from "./Sidebar"
 import { useConversation } from "../context/ConversationProvider"
 import { OpenConversation } from "./OpenConversation"
 export const Dashboard = (id) => {
+    const [showConversation, setShowConversation] = useState(true);
+
     const { selectedConversation } = useConversation()
     return (
 
         <div style={{ display: "flex" }}>
-            <Sidebar id={id} />
-            {selectedConversation && <OpenConversation />}
+            <Sidebar id={id} setShowConversation={setShowConversation} />
+            {selectedConversation && showConversation && <OpenConversation />}
         </div>
     )
 }
