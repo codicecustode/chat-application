3e418f1a4e34f49c829843410a06fc49eabcb70b
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-11-13 21:43:51 +0530

    Wrap application components with SocketProvider for WebSocket management

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,19 +2,25 @@ import { useLocalStorage } from '../hooks/useLocalStorage';
 import { Login } from "./Login"
 import { Dashboard } from './Dashboard'
 import { ModelShowProvider } from '../context/ModelShowContext'
-import { ContactsProvider} from '../context/ContactsProvider'
+import { ContactsProvider } from '../context/ContactsProvider'
 import { ConversationProvider } from '../context/ConversationProvider'
+import { SocketProvider } from '../context/SocketProvider';
 function App() {
   const [id, setId] = useLocalStorage('id')
 
   return (
-    <ContactsProvider>
-      <ConversationProvider id={id}>
-        <ModelShowProvider>
-          {id ? <Dashboard id={id} /> : <Login onSubmitId={setId} />}
-        </ ModelShowProvider>
-      </ConversationProvider>
-    </ContactsProvider>
+    <SocketProvider>
+
+      <ContactsProvider id={id}>
+        <ConversationProvider id={id}>
+          <ModelShowProvider>
+            {id ? <Dashboard id={id} /> : <Login onSubmitId={setId} />}
+          </ ModelShowProvider>
+        </ConversationProvider>
+      </ContactsProvider>
+
+    </SocketProvider>
+
   );
 }
 
